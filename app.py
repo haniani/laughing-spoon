e00eb2835748af8c7c14ee973a2d8f2d96c6ea2d
haniani <haniani@haniani>
2016-10-28 02:27:41 +0300

    flasking in / flasking out

---
+++ app.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+
+import os 
+import fnmatch
+import locale
+
+import flask
+from flask import render_template
+from flask import Flask
+from flask import Markup
+from flask import abort
+from flask import jsonify
+from flask import url_for
+from flask import redirect 
+from flask import request
+from flask import json
+
+from datetime import datetime
+from markdown import markdown
+
+
+try:
+    from os import getuid
+
+except ImportError:
+    def getuid():
+        return 4000
+
+app = Flask(__name__)
+app.config['ASSETS_DEBUG'] = True
+app.debug = True
+
+locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')  #для дат на русском
+
+def format_post(item):                                        #форматируем текст с markdown
+    bits = item.split('_', 1)
+    date = datetime.strptime(bits[0], '%Y%m%d')
+    title = bits[1].replace('_', ' ').replace('.md', '').title()
+    slug = title.lower().replace(' ', '-')
+    
+    return {'date': date, 'title': title, 'slug': slug}
+
+def get_post_dir():
+    return os.path.dirname(os.path.abspath(__file__)) + '/posts'  #определяем папку с постами
+
+def get_post_items():             #находим все посты и сортируем 
+    items = os.listdir(get_post_dir())
+    items.sort(reverse=True)    #сортируем от более новых к старым
+    return items
+
+def get_posts():     #получаем все посты
+    posts = []
+    for item in get_post_items():
+        if item[0] == '.' or item[0] == '_':
+            continue
+        post = format_post(item)
+        posts.append(post)
+
+    return posts
+
+@app.template_filter('date_format')    #человеческий вид даты в шаблоне
+def date_format(timestamp):
+    return timestamp.strftime('{S} %B, %Y').replace('{S}', str(timestamp.day))
+
+@app.errorhandler(404)                                      #для ошибок
+def _404(e):
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(500)
+def _500(e):
+    return render_template('errors/500.html'), 500
+
+
+
+@app.route('/')
+@app.route('/index')       #главная страница
+def index():
+
+    try:
+        template = render_template('index.html', posts=get_posts())
+        return template
+    except:
+        abort(500)
+
+@app.route("/gusinafamily")
+def gusinafamily():
+    return flask.render_template('gusinafamily.html')
+
+@app.route("/about")
+def about():
+    return flask.render_template('about.html')
+
+@app.route("/screenshots")
+def screenshots():
+    return flask.render_template('screenshots.html')
+
+
+@app.route('/page/<slug>')    #статика
+def page(slug):
+    try:
+        content = app.open_resource('pages/%s.md' % slug.replace('-', '_'), 'r').read()
+        content = Markup(markdown(content))
+
+        title = slug.replace('-', ' ').title()
+        template = render_template('page.html', content=content, page_title=title)
+
+        return template
+    except:
+        abort(404)
+
+
+@app.route('/post/<slug>')    #посты в блоге
+def post(slug):
+    try:
+        post = None
+        for item in get_post_items():
+            if fnmatch.fnmatch(item, '*_%s.md' % slug.replace('-', '_')):
+                post = item
+                break
+
+        content = app.open_resource('posts/%s' % post, 'r').read()
+        content = Markup(markdown(content))
+        title = format_post(post)['title']
+        date = format_post(post)['date']
+        template = render_template('post.html', content=content, page_title=title, post_date=date)
+
+        return template
+    except:
+        abort(404)
+
+@app.route('/post_len', methods=['GET', 'POST'])
+def post_len():
+    count = request.form['counter']
+    if len(count) > 140:
+        return json.dumps({'len': str(len(count))+ " <font color=\"red\">#not_for_twitter</font>"})
+    else:
+        return json.dumps({'len': str(len(count))+ " <font color=\"green\">#good_for_twitter</font>"})
+
+if __name__ == '__main__':
+    app.run()
---
+++ posts/20161027_hello_gusina.md
@@ -0,0 +1,13 @@
+Hello, Gusina
+================
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et elit tellus. Duis facilisis ornare aliquam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse nisl tellus, varius vel viverra vel, ultrices eget ante. Praesent tempus, dui a ultricies tempor, neque dui mattis nisl, vitae euismod nisl lorem sit amet nisl. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam eu justo suscipit, ultricies orci sit amet, feugiat sem. Mauris non laoreet nisi. Curabitur quis dapibus felis. Fusce vel nisi maximus, suscipit ex sit amet, hendrerit est. Duis ac varius enim, nec tincidunt justo. Sed a semper dolor, quis ultricies diam.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet blandit tortor, sodales auctor eros. Vestibulum eu consequat augue. Aenean porta lorem ac orci facilisis, vel iaculis tellus condimentum. Quisque cursus augue et sodales gravida. Suspendisse sed eros viverra, lacinia augue sed, pulvinar velit. Curabitur fermentum nunc ipsum, id accumsan nibh pellentesque et. Praesent ligula elit, tempor faucibus nunc sed, mattis porttitor nisi.
+
+Morbi ornare aliquet pellentesque. Aenean nisi nunc, ullamcorper nec pretium ut, fringilla et tortor. Etiam eleifend eget sapien ut elementum. Pellentesque eu gravida sapien, non molestie augue. Morbi molestie ante tortor, a consequat nisi accumsan ac. Mauris volutpat justo vel tempor porttitor. Etiam ullamcorper id lectus in vehicula. Nam dictum est quis ante luctus hendrerit. Cras sit amet quam feugiat, tincidunt nulla vel, sagittis purus. Donec semper pharetra augue. Pellentesque in odio a leo aliquet faucibus. Cras sollicitudin ligula quis nisl tincidunt, non ornare ex sodales.
+
+
+Bye!
+----
+
